b78ed8ceb09e40b2d1f805afb3ad89eff9105892
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-02 23:11:23 +0000

    Add abstract ApiRequest

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\ApiRequest;
 
-class StoreUserRequest extends ApiRequest
+class UpdateUserRequest extends ApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
