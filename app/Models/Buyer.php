fdd918337754f906c0ed904f149307a0c8ae95e9
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-03 23:04:59 +0000

    Add BuyerScope implementation in Buyer model

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -3,9 +3,25 @@
 namespace App\Models;
 
 use App\Models\Transaction;
+use App\Scopes\BuyerScope;
 
 final class Buyer extends User
 {
+    /**
+     * Overload the boot method to add custom Global scope.
+     * 
+     * BuyerScope adds constraint has('transactions') to all
+     * queries.
+     *
+     * @return void
+     */
+    protected static function boot() 
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
+
     /**
      * Define the transactions relationship
      * 
