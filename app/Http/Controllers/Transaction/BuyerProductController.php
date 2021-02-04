f12a653462a449000e0d1ee309cb8312be428567
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-04 22:42:56 +0000

    Add pluck to return only the product in BuyerProduct index

--- app/Http/Controllers/Transaction/BuyerProductController.php
+++ app/Http/Controllers/Transaction/BuyerProductController.php
@@ -14,7 +14,10 @@ class BuyerProductController extends ApiController
      */
     public function index(Buyer $buyer)
     {
-        $products = $buyer->transactions()->with('product')->get();
+        $products = $buyer->transactions()
+                        ->with('product')
+                        ->get()
+                        ->pluck('product');
 
         return $this->showAll($products);
     }
