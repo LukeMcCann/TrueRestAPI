df5d5d57879817c7e63fe64e928cc03c8ace3417
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-05 18:26:16 +0000

    Add SellerCategories controller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Category\CategoryProductController;
 use App\Http\Controllers\Category\CategorySellerController;
 use App\Http\Controllers\Category\CategoryTransactionController;
 use App\Http\Controllers\Category\CategoryBuyerController;
+use App\Http\Controllers\Seller\SellerCategoryController;
 use App\Http\Controllers\Seller\SellerTransactionController;
 use App\Http\Controllers\Transaction\BuyerProductController;
 use App\Http\Controllers\Transaction\TransactionCategoryController;
@@ -40,6 +41,7 @@ Route::resource('buyers.categories', BuyerCategoryController::class)->only(['ind
 // Sellers
 Route::resource('sellers', SellerController::class)->only(['index', 'show']);
 Route::resource('sellers.transactions', SellerTransactionController::class)->only(['index']);
+Route::resource('sellers.categories', SellerCategoryController::class)->only(['index']);
 
 // Categories
 Route::resource('categories', CategoryController::class)->except(['create', 'edit']);
