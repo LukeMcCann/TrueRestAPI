9db282cf8da3941a58e379768814be140c258526
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-05 21:21:41 +0000

    Add ProductCategory destroy

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ApiController;
 use App\Http\Requests\UpdateProductCategoryRequest;
 use App\Models\Product;
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 
 class ProductCategoryController extends ApiController
 {
@@ -33,5 +34,41 @@ class ProductCategoryController extends ApiController
         $product->categories()->syncWithoutDetaching([$category->id]);
 
         return $this->showAll($product->categories);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product, Category $category) 
+    {
+        $this->checkCategory($product, $category);
+
+        $product->categories()->detach($category->id);
+
+        return $this->showAll(
+            $product->categories,
+            JsonResponse::HTTP_NO_CONTENT
+        );
+    }
+
+    /**
+     * Validates the category is the assigned category of the specified product.
+     *
+     * @param Seller $seller
+     * @param Product $product
+     * 
+     * @return void
+     */
+    private function checkCategory(Product $product, Category $category)
+    {
+        if (!$product->categories()->find($category->id)) {
+            throw new HttpException(
+                JsonResponse::HTTP_UNPROCESSABLE_ENTITY,
+                'The category specified is not an actual category assigned to the product.'
+            );
+        }
     }
 }
