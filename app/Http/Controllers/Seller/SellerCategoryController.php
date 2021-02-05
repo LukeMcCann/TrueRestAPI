785070ff32978b554712be59cce3f0f7df91e54a
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-05 18:35:52 +0000

    Add SellerBuyerController

--- app/Http/Controllers/Seller/SellerCategoryController.php
+++ app/Http/Controllers/Seller/SellerCategoryController.php
@@ -15,6 +15,7 @@ class SellerCategoryController extends ApiController
     public function index(Seller $seller)
     {
         $categories = $seller->products()
+                             ->whereHas('categories')
                              ->with('categories')
                              ->get()
                              ->pluck('categories')
